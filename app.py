b4bb6b43f7515140d049f532943ee9a7fceeac3f
alejandravillanueva-lab <alejandravillanueva@ciencias.unam.mx>
2025-12-08 23:20:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -312,15 +312,25 @@ def markowitz_target_portfolio(mu, cov, target_anual):
 
 def main():
     st.markdown("## 📈 Cálculo de Métricas de Portafolios")
-    st.markdown(
-        """
-        Aplicación para analizar portafolios de **Regiones** y **Sectores**:
+   st.markdown("""
+<div style="
+    background-color:#ffffff;
+    padding:18px;
+    border-radius:10px;
+    border-left:4px solid #7a3db8;
+    box-shadow:0px 2px 6px rgba(0,0,0,0.05);
+    margin-bottom:20px;">
+    <p style="font-size:1rem; color:#333;">
+        Aplicación para analizar portafolios de <b>Regiones</b> y <b>Sectores</b>:
+    </p>
+    <ul style="font-size:1rem; color:#333; line-height:1.6;">
+        <li>Benchmark (pesos dados)</li>
+        <li>Portafolio arbitrario (definido por el usuario)</li>
+        <li>Portafolios optimizados: mínima varianza, máximo Sharpe y Markowitz con rendimiento objetivo.</li>
+    </ul>
+</div>
+""", unsafe_allow_html=True)
 
-        - Benchmark (pesos dados)  
-        - Portafolio arbitrario (definido por el usuario)  
-        - Portafolios optimizados: mínima varianza, máximo Sharpe y Markowitz con rendimiento objetivo.
-        """
-    )
 
     # Sidebar: parámetros generales
     estrategia = st.sidebar.selectbox("Estrategia", ["Regiones", "Sectores"])
